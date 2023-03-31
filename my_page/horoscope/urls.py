a2970f50e1e96a3ca988dfb93a132984b265a615
mitatskni <mitatskni@gmail.com>
2023-03-31 16:12:40 +0200

    Add path to main menu horoscope

--- my_page/horoscope/urls.py
+++ my_page/horoscope/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('', views.index),
     path('<int:sign_of_zodiac>/', views.get_horoscope_by_num),
     path('<str:sign_of_zodiac>/', views.get_horoscope_by_sign, name = 'horoscope-name')
 ]
