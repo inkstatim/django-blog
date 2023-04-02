c75348bae2e1a0a8704d057677aff3cbdd071ea6
mitatskni <mitatskni@gmail.com>
2023-04-02 13:02:39 +0200

    Learning different Django tags

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+def main(request):
+    return HttpResponse('Главная страница')
 
-# Create your views here.
+def get_post(request):
+    return render(request, 'blog/list_detail.html')
+def get_posts(request, name_post):
+    data = {
+        'post':name_post
+    }
+    return  render(request, 'blog/detail_by_name.html', context=data)
+def get_info_about_num(request, num: int):
+    data = {
+        'number' : num
+    }
+    return render(request, 'blog/detail_by_number.html', context=data)
+
+def index_html(request):
+    response = render_to_string('blog/index.html')
+    return HttpResponse(response)
