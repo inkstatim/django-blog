ad952992b58f4e40f7ced6589f2984a4ca34120b
mitatskni <mitatskni@gmail.com>
2023-04-02 12:57:46 +0200

    Added dynamic url for zodiac signs

--- my_page/horoscope/urls.py
+++ my_page/horoscope/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('type/', views.type_sign),
+    path('type/<str:type_name>/', views.type, name='types_names'),
     path('<int:sign_of_zodiac>/', views.get_horoscope_by_num),
     path('<str:sign_of_zodiac>/', views.get_horoscope_by_sign, name = 'horoscope-name')
 ]
+
+
+
+
