895c621f798c2574ec27633eb629a9ea2cdd3739
mitatskni <mitatskni@gmail.com>
2023-04-02 13:00:50 +0200

    Created a test application and added url

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -1,6 +1,7 @@
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.main),
+    path('posts/<int:num>/', views.get_info_about_num),
+    path('posts/<str:name_post>/', views.get_posts),
+]
