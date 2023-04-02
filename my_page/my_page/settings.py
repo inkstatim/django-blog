6c7ae62c9183d8d311bf2ff5c6806f91db580706
mitatskni <mitatskni@gmail.com>
2023-04-02 13:03:12 +0200

    Added template

--- my_page/my_page/settings.py
+++ my_page/my_page/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'horoscope',
+    'test_some_feature',
 ]
 
 MIDDLEWARE = [
