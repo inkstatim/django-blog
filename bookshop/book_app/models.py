033c9ad6e9d721497888e72f59a345d4d5747d89
mitatskni <mitatskni@gmail.com>
2023-04-04 23:06:36 +0200

    Create module

--- bookshop/book_app/models.py
+++ bookshop/book_app/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+
 class Book(models.Model):
     title = models.CharField(max_length=70)
-    rating = models.IntegerField
+    rating = models.IntegerField()
+    is_best_selling = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} - { self.rating}%"
--- movie_proj/movie_app/models.py
+++ movie_proj/movie_app/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Movie(models.Model):
     name = models.CharField(max_length=40)
-    rating = models.IntegerField
+    rating = models.IntegerField()
