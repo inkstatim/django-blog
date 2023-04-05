92182217d3065d0819119c20496b871a7afe4462
mitatskni <mitatskni@gmail.com>
2023-04-05 13:34:29 +0200

    add to Class Book column author

--- bookshop/book_app/models.py
+++ bookshop/book_app/models.py
@@ -5,6 +5,7 @@ class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
     is_best_selling = models.BooleanField(default=False)
+    author= models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return f"{self.title} - { self.rating}%"
