f07f4e90b3880fa677b331ad8ed8feb0fea00f62
mitatskni <mitatskni@gmail.com>
2023-04-07 12:17:32 +0200

    create func in views

--- movie_proj/movie_app/views.py
+++ movie_proj/movie_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Movie
+
 
 # Create your views here.
+
+def show_all_movie(request):
+    movies = Movie.objects.all()
+    return render(request, 'movie_app/all_movies.html', {'movies': movies})
+
+
+def show_one_movie(request, slug_movie: str):
+    movie = Movie.objects.get(slug=slug_movie)
+    return render(request, 'movie_app/one_movie.html', {'movie': movie})
