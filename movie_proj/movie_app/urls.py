22b4f04900851fa9d16723cdf256fea4fc185938
mitatskni <mitatskni@gmail.com>
2023-04-07 12:17:08 +0200

    add path movie

--- movie_proj/movie_app/urls.py
+++ movie_proj/movie_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.show_all_movie)
+    path('', views.show_all_movie),
+    path('movie/<slug:slug_movie>', views.show_one_movie, name='movie-detail'),
 ]
