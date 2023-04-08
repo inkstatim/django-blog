bfa2bfd02cc9d740688e0d8afe6f99a2403d3c87
mitatskni <mitatskni@gmail.com>
2023-04-08 13:43:22 +0200

    added new column to CURRENCY_CHOICES

--- movie_proj/movie_app/models.py
+++ movie_proj/movie_app/models.py
@@ -2,10 +2,20 @@ from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 class Movie(models.Model):
+    EUR = 'EUR'
+    USD = 'USD'
+    UAH = 'UAH'
+    CURRENCY_CHOICES = [
+        (EUR, 'Euro'),
+        (USD, 'Dollar'),
+        (UAH, 'Hrivnya'),
+    ]
+
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
     year = models.IntegerField(null=True, blank=True)
     budget = models.IntegerField(default=100000)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=UAH)
     slug = models.SlugField(default='', null=False)
 
 
