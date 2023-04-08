3bed23c32c2e1b5d8a5e4ec22eaf0666b44e2254
mitatskni <mitatskni@gmail.com>
2023-04-08 09:18:50 +0200

    added my column to the admin panel

--- movie_proj/movie_app/admin.py
+++ movie_proj/movie_app/admin.py
@@ -5,5 +5,17 @@ from .models import Movie
 # Register your models here.
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'year', 'budget']
+    list_display = ['name', 'rating', 'year', 'budget', 'rating_status']
     list_editable = ['rating', 'year', 'budget']
+    ordering = ['-rating', '-name']
+    list_per_page = 10
+
+    @admin.display(ordering='rating', description='Статус')
+    def rating_status(self, mov: Movie):
+        if mov.rating < 50:
+            return "Лучше не смотреть!!!"
+        if mov.rating < 70:
+            return "Нормально"
+        if mov.rating <= 85:
+            return "Зачет"
+        return "Топчик"
