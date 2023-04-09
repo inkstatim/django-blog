504578d98d52ef2cb074e550cbee5a3377fa0ca0
mitatskni <mitatskni@gmail.com>
2023-04-09 15:03:58 +0200

    create new patch for director and actors

--- movie_proj/movie_app/urls.py
+++ movie_proj/movie_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 urlpatterns = [
     path('', views.show_all_movie),
     path('movie/<slug:slug_movie>', views.show_one_movie, name='movie-detail'),
+    path('directors', views.ddirectors),
+    path('directors/<slug:slug_name>', views.name_director, name='dir-detail'),
+    path('actors', views.info_actors),
+    path('actors/<int:id_act>', views.info_actor, name='actor-detail'),
+
 ]
--- movie_proj/movie_app/views.py
+++ movie_proj/movie_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Movie
+from django.shortcuts import render, get_object_or_404
+from .models import Movie, Director, Actor
 
 
 # Create your views here.
@@ -12,3 +12,25 @@ def show_all_movie(request):
 def show_one_movie(request, slug_movie: str):
     movie = Movie.objects.get(slug=slug_movie)
     return render(request, 'movie_app/one_movie.html', {'movie': movie})
+
+
+def ddirectors(request):
+    directors = Director.objects.all()
+    for dir in directors:
+        dir.save()
+    return render(request, 'movie_app/director.html', {'directors': directors})
+
+
+def name_director(request, slug_name: str):
+    name_dir = get_object_or_404(Director, slug=slug_name)
+    return render(request, 'movie_app/info_dir.html', {'name_dir': name_dir})
+
+
+def info_actors(request):
+    actors = Actor.objects.all()
+    return render(request, 'movie_app/actors.html', {'actors':actors})
+
+
+def info_actor(request, id_act: int):
+    actor = get_object_or_404(Actor, id=id_act)
+    return render(request, 'movie_app/actor.html', {'actor':actor})
