f15aa745bcbe6efbf5544c563f5ecd7bc1901a70
mitatskni <mitatskni@gmail.com>
2023-04-09 15:03:31 +0200

    set ЙцУкЕн

--- movie_proj/movie_app/models.py
+++ movie_proj/movie_app/models.py
@@ -1,6 +1,46 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
+
+
+class Director(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    director_email = models.EmailField()
+    slug = models.SlugField(default='', null=False)
+
+    def __str__(self):
+        return f"{self.first_name}  {self.last_name}"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.first_name)
+        super(Director, self).save(*args, **kwargs)
+
+    def get_url(self):
+        return reverse('dir-detail', args=[self.slug])
+
+
+class Actor(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDERS = [
+        (MALE, 'Мужчина'),
+        (FEMALE, 'Женщина')
+    ]
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=1, choices=GENDERS, default=MALE)
+
+    def __str__(self):
+        if self.gender == self.MALE:
+            return f"Актер {self.first_name} {self.last_name}"
+        else:
+            return f"Актриса {self.first_name} {self.last_name}"
+
+    def get_url(self):
+        return reverse('actor-detail', args=[self.id])
+
+
 class Movie(models.Model):
     EUR = 'EUR'
     USD = 'USD'
@@ -17,11 +57,13 @@ class Movie(models.Model):
     budget = models.IntegerField(default=100000)
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=UAH)
     slug = models.SlugField(default='', null=False)
-
+    director = models.ForeignKey(Director, on_delete=models.SET_NULL, null=True)
+    actors = models.ManyToManyField(Actor)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Movie, self).save(*args, **kwargs)
+
     def get_url(self):
         return reverse('movie-detail', args=[self.slug])
 
