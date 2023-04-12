d78dacce82803346e79aa8f795d84aae7af61050
mitatskni <mitatskni@gmail.com>
2023-04-12 11:33:29 +0200

    rename path

--- form_project/feedback/urls.py
+++ form_project/feedback/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('list_feed', views.ListFeedBack.as_view()),
+    path('list', views.ListFeedBack.as_view()),
     path('detail/<id_feedback>', views.DetailFeedBack.as_view()),
     path('done', views.DoneView.as_view()),
     path('', views.FeedbackView.as_view()),
