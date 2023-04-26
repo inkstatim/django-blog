f407d76fc177a9e7759bdb83780ced8e37c7bfd5
mitatskni <mitatskni@gmail.com>
2023-04-26 21:02:08 +0200

    Initial commit

---
+++ mysite/blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.http import Http404
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/list.html', {'posts': posts})
+
+
+# Create your views here.
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+
+    return render(request, 'blog/detail.html', {'post': post})
