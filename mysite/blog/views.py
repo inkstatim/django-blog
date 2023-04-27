fdf18aa3dc97b7b92470c84919c588fec4bdc7a2
mitatskni <mitatskni@gmail.com>
2023-04-27 17:36:01 +0200

    Добавление комментариев в представление детальной информации о посте

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -32,8 +32,11 @@ def post_list(request):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year,
                              publish__month=month, publish__day=day)
-
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # Список активных комментариев к этому посту
+    comments= post.comments.filter(active=True)
+    # Форма для комментирования пользователями
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post,'comments': comments, 'form': form})
 
 
 def post_share(request, post_id):
