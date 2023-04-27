49e9df570f8ace3804a4c46b04d89312de2deaae
mitatskni <mitatskni@gmail.com>
2023-04-27 16:35:08 +0200

    Отправка электронных писем в представлениях

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -37,12 +38,18 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
     # Извлечь пост по идентификатору id
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == "POST":
         # Форма была передана в обработку
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'your_account@gmail.com',[cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
 
-    return render(request, 'blog/share.html', {'post':post, 'form':form})
+    return render(request, 'blog/share.html', {'post': post, 'form': form, 'sent': sent})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -129,3 +129,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Конфигурация сервера электронной почты
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = local_settings.EMAIL_HOST_USER
+# EMAIL_HOST_PASSWORD = local_settings.EMAIL_HOST_PASSWORD
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
